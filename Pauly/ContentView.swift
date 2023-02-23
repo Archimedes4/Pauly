3ff60c0f14790ec176491694118e50708e070ced
Archimedes4 <82121191+AMCanada16@users.noreply.github.com>
2023-02-23 14:33:56 -0600

    Init

--- Pauly/ContentView.swift
+++ Pauly/ContentView.swift
@@ -7,16 +7,89 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+enum WindowSrceens{
+    case PasswordWindow
+    case TurorForm
+    case TurtorChat
+    case HomePage
+    case Quiz
+    case Calendar
+}
+
+struct Course{
+    let Course: String
+    let Grade: [Int]
+    let Teacher: String
+}
+
+struct QuizView: View{
+    @Binding var WindowMode: WindowSrceens
+    var body: some View{
+        VStack{
+            Text("Quizes")
+            Button("Back"){
+                WindowMode = .HomePage
+            }
+        }.background(Color.marron)
     }
+}
+
+struct HomePage: View{
+    @Binding var WindowMode: WindowSrceens
+    var body: some View{
+        Text("Home Page")
+        Button("Quizes"){
+            WindowMode = .Quiz
+        }
+        Button("Log Out"){
+            WindowMode = .PasswordWindow
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var WindowMode: WindowSrceens = .PasswordWindow
+    var body: some View {
+        if WindowMode == .PasswordWindow{
+            PasswordView(WindowMode: $WindowMode)
+        }
+        if WindowMode == .HomePage{
+            HomePage(WindowMode: $WindowMode)
+        }
+        if WindowMode == .Quiz{
+            QuizView(WindowMode: $WindowMode)
+        }
+    }
+}
+
+struct PasswordView: View{
+    @Binding var WindowMode: WindowSrceens
+    
+    @State var Username: String = ""
+    @State var Password: String = ""
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .foregroundColor(Color.marron)
+            VStack {
+                Text("Login")
+                TextField("Username", text: $Username)
+                    .background(Color.white)
+                    .frame(minWidth: 50.0, idealWidth: 90.0, maxWidth: 120.0, minHeight: 50.0, idealHeight: 90.0, maxHeight: 120.0, alignment: .center)
+        
+                SecureField("Password", text: $Password)
+                    .background(Color.white)
+                    .frame(minWidth: 50.0, idealWidth: 90.0, maxWidth: 120.0, minHeight: 50.0, idealHeight: 90.0, maxHeight: 120.0, alignment: .center)
+                    .onSubmit {
+                        print("Test")
+                        WindowMode = .HomePage
+                    }
+            }
+        }.edgesIgnoringSafeArea(.all)
+    }
+}
+extension Color {
+    static let marron = Color("Marron")
 }
 
 struct ContentView_Previews: PreviewProvider {
