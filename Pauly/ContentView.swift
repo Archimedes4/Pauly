3de465f98c55d51f49216a86e1207f530f032638
Archimedes4 <82121191+AMCanada16@users.noreply.github.com>
2023-02-23 12:16:24 -0600

    Initial Commit

---
+++ Pauly/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pauly
+//
+//  Created by Andrew Mainella on 2023-02-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pauly/PaulyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaulyApp.swift
+//  Pauly
+//
+//  Created by Andrew Mainella on 2023-02-23.
+//
+
+import SwiftUI
+
+@main
+struct PaulyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
