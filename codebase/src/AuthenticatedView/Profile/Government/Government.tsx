924a10d2d3e16fe6047a7d13ec3bf93db0681b2d
Archimedes4 <82121191+AMCanada16@users.noreply.github.com>
2023-08-04 15:23:00 -0500

    adding estitics to government

--- codebase/src/AuthenticatedView/Profile/Government/Government.tsx
+++ codebase/src/AuthenticatedView/Profile/Government/Government.tsx
@@ -1,20 +1,38 @@
-import { View, Text, Dimensions } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { View, Text, Dimensions, Image, ImageSourcePropType } from 'react-native'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-native'
 import NavBarComponent from '../../../UI/NavComponent'
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { accessTokenContent } from '../../../../App';
 
-function Block() {
+function Block({height, width, text, imageSource}:{height: number, width: number, text: string, imageSource?: ImageSourcePropType}) {
   return (
-    <View>
-      
+    <View style={{height: height, width: width, alignContent: "center", alignItems: "center", justifyContent: "center"}}>
+      { (imageSource !== undefined) && <Image source={imageSource} style={{width: width * 0.5, height: height * 0.5}}/>}
+      <Text>{text}</Text>
     </View>
   )
 }
 
 export default function Government() {
+  const [fontsLoaded] = useFonts({
+    'BukhariScript': require('../../../../assets/fonts/BukhariScript.ttf'),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  const pageData = useContext(accessTokenContent);
+  if (!fontsLoaded) {
+    return null
+  }
   return (
     <View>
-      <Text>Government</Text>
+      <View style={{height: pageData.dimensions.window.height * 0.2, width: pageData.dimensions.window.width, alignContent: "center", justifyContent: "center", alignItems: "center", backgroundColor: "#444444"}}>
+        <Text style={{fontFamily: "BukhariScript", fontSize: 100}}>Government</Text>
+      </View>
       <Link to="/profile/government/graph">
         <Text>Microsoft Graph</Text>
       </Link>
@@ -22,7 +40,7 @@ export default function Government() {
         <Text>Commissions</Text>
       </Link>
       <Link to="/profile/government/sports">
-        <Text>Sports</Text>
+        <Block width={100} height={100} text='Sports' imageSource={require('../../../../assets/images/Football.png')} />
       </Link>
       <Link to="/profile/government/president">
         <Text>President</Text>
