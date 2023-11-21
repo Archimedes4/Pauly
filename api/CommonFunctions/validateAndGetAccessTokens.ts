ac99c0acee93a207358644ca35a76e9de7356e91
Archimedes4 <82121191+AMCanada16@users.noreply.github.com>
2023-11-21 06:57:46 -0600

    bug fix

--- api/CommonFunctions/validateAndGetAccessTokens.ts
+++ api/CommonFunctions/validateAndGetAccessTokens.ts
@@ -75,8 +75,11 @@ const getSigningKeys = (header: JwtHeader, callback: SigningKeyCallback) => {
  */
 const validateToken = (req: HttpRequest): Promise<string> => {
   return new Promise(async (resolve, reject) => {
+    console.log('MARK 1.1.1')
+    console.log(req.headers.get('Authorization'))
     const authHeader = req.headers.get('Authorization');
     if (authHeader) {
+      console.log('MARK 1.1')
       const token = authHeader.split(' ').pop();
 
       const validationOptions = {
@@ -84,14 +87,17 @@ const validateToken = (req: HttpRequest): Promise<string> => {
       };
 
       verify(token, getSigningKeys, validationOptions, (err, payload) => {
+        console.log('MARK 1.3')
         if (err) {
+          console.log('MARK 1.4')
           reject(403);
           return;
         }
-
+        console.log('MARK 1.5')
         resolve(token);
       });
     } else {
+      console.log('MARK 1.2')
       reject(401);
     }
   });
