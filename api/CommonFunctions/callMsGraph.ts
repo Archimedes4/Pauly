e7de7917355470df4d38146ba62e748c3be2efd8
Archimedes4 <82121191+AMCanada16@users.noreply.github.com>
2023-11-21 13:40:21 -0600

    updating commmissions

--- api/CommonFunctions/callMsGraph.ts
+++ api/CommonFunctions/callMsGraph.ts
@@ -1,19 +1,22 @@
-export default async function callMsGraph(authenticationToken: string, url: string, method?: "GET" | "POST" | "PATCH" | "DELETE" | "PUT", body?: string): Promise<Response> {
+export default async function callMsGraph(
+  authenticationToken: string,
+  url: string,
+  method?: 'GET' | 'POST' | 'PATCH' | 'DELETE' | 'PUT',
+  body?: string,
+): Promise<Response> {
   const headers = new Headers();
-  const bearer = `Bearer ${authenticationToken}`
+  const bearer = `Bearer ${authenticationToken}`;
 
-  headers.append("Authorization", bearer);
-  headers.append("Content-Type", "application/json")
+  headers.append('Authorization', bearer);
+  headers.append('Content-Type', 'application/json');
 
   const options = {
-    method: (method) ? method:"GET",
-    headers: headers,
-    body: body
+    method: method || 'GET',
+    headers,
+    body,
   };
   try {
-    const response  = await fetch(url, options)
-    return response
-  } catch (e) {
-    console.log(e)
-  }
+    const response = await fetch(url, options);
+    return response;
+  } catch (e) {}
 }
--- api/CommonFunctions/getPaulyList.ts
+++ api/CommonFunctions/getPaulyList.ts
@@ -1,38 +1,56 @@
-import { Context, HttpRequest } from "@azure/functions";
-import callMsGraph from "./callMsGraph";
+import { HttpResponseInit } from '@azure/functions';
+import callMsGraph from './callMsGraph';
 
 declare global {
   type listResponce = {
-    siteId: string,
-    commissionListId: string,
-    userExtensionId: string,
-    commissionSubmissionsListId: string
-  }
+    siteId: string;
+    commissionListId: string;
+    userExtensionId: string;
+    commissionSubmissionsListId: string;
+  };
 }
 
-export default async function getPaulyList(context: Context, req: HttpRequest, accessToken: string): Promise<listResponce | undefined> {
-    //Get Site Data
-    const orgWideGroupId = (req.query.orgWideGroupId || (req.body && req.body.orgWideGroupId));
-    if (orgWideGroupId === undefined){context.res = {status: 400,body: "Error: orgWideGroupId needs to be supplied"}; return}
-    
-    const getRootSiteIdResult = await callMsGraph(accessToken, "https://graph.microsoft.com/v1.0/groups/" + orgWideGroupId + "/sites/root")
-    if (!getRootSiteIdResult.ok){context.res = {status: 500,body: "Internal Error: Unable To Get Site"}; return}
-    const getRootSiteIdResultData = await getRootSiteIdResult.json()
+const orgWideGroupID = 'd2c0dd90-a426-4539-882c-aa87e5787a92';
 
-    if (getRootSiteIdResultData["id"] === undefined) {context.res = {status: 500,body: "Internal Error: Unable To Get Site"}; return}
-    const paulyListResult = await callMsGraph(accessToken, "https://graph.microsoft.com/v1.0/sites/" + getRootSiteIdResultData["id"] + "/lists/PaulyList/items?expand=fields")
-    const paulyListResultData = await paulyListResult.json()
-    if (!paulyListResult.ok){context.res = {status: 500, body: "Internal Error: Unable To Get Ids"}; return}
-    try {
-      if (paulyListResultData["value"].length !== 1) {context.res = {status: 500, body: "Internal Error: Unable To Get Ids"}; return}
-    } catch {
-      context.res = {status: 500, body: "Internal Error: Unable To Get Ids"}; return
-    }
+export default async function getPaulyList(
+  accessToken: string,
+): Promise<listResponce | HttpResponseInit> {
+  // Get Site Data
 
-    return {
-      siteId:  getRootSiteIdResultData["id"],
-      commissionListId: paulyListResultData["value"][0]["fields"]["commissionListId"],
-      userExtensionId: paulyListResultData["value"][0]["fields"]["userExtensionId"],
-      commissionSubmissionsListId: paulyListResultData["value"][0]["fields"]["commissionSubmissionsListId"]
+  const getRootSiteIdResult = await callMsGraph(
+    accessToken,
+    `https://graph.microsoft.com/v1.0/groups/${orgWideGroupID}/sites/root`,
+  );
+  if (!getRootSiteIdResult.ok) {
+    return { status: 500, body: 'Internal Error: Unable To Get Site' };
+  }
+  const getRootSiteIdResultData = await getRootSiteIdResult.json();
+
+  if (getRootSiteIdResultData.id === undefined) {
+    return { status: 500, body: 'Internal Error: Unable To Get Site' };
+    return;
+  }
+  const paulyListResult = await callMsGraph(
+    accessToken,
+    `https://graph.microsoft.com/v1.0/sites/${getRootSiteIdResultData.id}/lists/PaulyList/items?expand=fields`,
+  );
+  const paulyListResultData = await paulyListResult.json();
+  if (!paulyListResult.ok) {
+    return { status: 500, body: 'Internal Error: Unable To Get Ids' };
+  }
+  try {
+    if (paulyListResultData.value.length !== 1) {
+      return { status: 500, body: 'Internal Error: Unable To Get Ids' };
     }
+  } catch {
+    return { status: 500, body: 'Internal Error: Unable To Get Ids' };
+  }
+
+  return {
+    siteId: getRootSiteIdResultData.id,
+    commissionListId: paulyListResultData.value[0].fields.commissionListId,
+    userExtensionId: paulyListResultData.value[0].fields.userExtensionId,
+    commissionSubmissionsListId:
+      paulyListResultData.value[0].fields.commissionSubmissionsListId,
+  };
 }
