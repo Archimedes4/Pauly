13cb647b08fff658e5833ddde03ffde6168c44d0
Archimedes4 <82121191+AMCanada16@users.noreply.github.com>
2024-04-12 15:45:22 -0500

    og meta implmented

--- src/app/+html.tsx
+++ src/app/+html.tsx
@@ -11,14 +11,13 @@ export default function Root({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <head>
+        {/* General Meta Tags */}
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="author" content="Andrew Mainella" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <meta name="apple-itunes-app" content="app-id=6445966725" />
         <link rel="manifest" href="/manifest.json" />
         <style type="text/css">
           {`
@@ -28,9 +27,21 @@ export default function Root({ children }: PropsWithChildren) {
         `}
         </style>
 
+        <meta name="apple-itunes-app" content="app-id=6445966725" />
+        {/* Preview Meta Tags*/}
+        <meta name="author" content="Andrew Mainella" />
+
+        {/* OG Meta Tags*/}
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Pauly" />
+        <meta property="og:description" content="Pauly, an app for students by students. Built by the 2023-2024 Student Council."></meta>
         <meta property="og:image" content={process.env.EXPO_PUBLIC_PAULYHOST + "/Pauly-og-Image.png"} />  
+
+        {/* Twitter Meta Tags*/}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Pauly" />
+        <meta name="twitter:description" content="Pauly, an app for students by students. Built by the 2023-2024 Student Council." />
+        <meta name="twitter:image" content="http://www.paulysphs.ca/Pauly-og-Image.png"/>
       </head>
       <body>{children}</body>
     </html>
