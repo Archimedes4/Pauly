8e13ae34b20308c875017fb7cc1fb233948cf130
Archimedes4 <82121191+AMCanada16@users.noreply.github.com>
2024-04-24 14:53:54 -0500

    updating override button

--- src/Components/LoadingComponent.tsx
+++ src/Components/LoadingComponent.tsx
@@ -1,12 +1,13 @@
 import { useSignOut } from '@hooks/authentication';
 import useIsShowingLogout from '@hooks/useIsShowingLogout';
-import { RootState } from '@redux/store';
+import store, { RootState } from '@redux/store';
 import { View, Text, Pressable } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useSelector } from 'react-redux';
 import React from 'react';
 import { Colors } from '@constants';
 import ProgressView from './ProgressView';
+import { isOverrideSlice } from '@src/redux/reducers/isOverrideReducer';
 
 export default function LoadingComponent() {
   const isGovernmentMode = useSelector(
@@ -33,8 +34,10 @@ export default function LoadingComponent() {
       <ProgressView width={14} height={14} />
       <Text style={{ color: Colors.white }}>Loading</Text>
       {isGovernmentMode ? (
-        <Pressable style={{ margin: 5 }}>
-          <Text style={{ color: Colors.white }}>Overide</Text>
+        <Pressable style={{ margin: 5, padding: 10, backgroundColor: Colors.white, borderRadius: 10 }} onPress={() => {
+          store.dispatch(isOverrideSlice.actions.setIsOverride(true))
+        }}>
+          <Text style={{ color: Colors.black }}>Overide</Text>
         </Pressable>
       ) : null}
       {isShowingLogout ? (
